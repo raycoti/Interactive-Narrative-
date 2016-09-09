683c0a354f1406dac0c6b1a605afb7d9e8a3b1b2
Raymond Coti <raymond.coti@gmail.com>
2016-09-09 02:14:26 -0500

    Add files via upload

---
+++ Assets/Standard%20Assets/Characters/FirstPersonCharacter/Scripts/SetUpLocalPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Networking;
+
+public class SetUpLocalPlayer : NetworkBehaviour {
+    public UnityEngine.GameObject child;
+    // Use this for initialization
+    void Start () {
+        
+        if (isLocalPlayer)
+        {
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>().enabled = true;
+            child = this.gameObject.transform.GetChild(0).gameObject;
+            child.GetComponent<UnityEngine.Camera>().enabled = true;
+            // GetComponent<UnityEngine.Camera>().enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	
+}
